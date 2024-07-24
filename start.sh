241af570457a001396ccc893d9feac607b251d18
Daniil Godovan <36306948+DanillGodovan@users.noreply.github.com>
2024-07-24 09:36:09 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-"/usr/lib/jvm/temurin-21-jdk-amd64/bin/java" -cp  ServerWrapper.jar -Xmx1024M -Xms1024M pro.gravit.launcher.server.ServerWrapper
+"/usr/lib/jvm/temurin-21-jdk-amd64/bin/java" -cp  ServerWrapper.jar pro.gravit.launcher.server.ServerWrapper
